c2fb9aa51d695de902fb39c5e8a4dc0f6936aabb
sai <155743645+saikiran055@users.noreply.github.com>
2025-10-09 15:26:42 +0530

    Create 23-25.py

---
+++ 23-25.py
@@ -0,0 +1,7 @@
+#Calculate the square of a number.
+#Calculate the cube of a number.
+#Find the square root of a number.
+a=int(input("enter number"))
+print(f"square of {a} is:{a**2}")
+print(f"cube of {a} is:{a**3}")
+print(f"square root of {a} is:{a**(1/2)}")
