7f70b5fc05f784d9b4d88cee27c259377ad5db28
sai <155743645+saikiran055@users.noreply.github.com>
2025-10-09 14:47:34 +0530

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,7 @@
+#Find the compound interest.
+p,r,t=input("Principal_amount,Annual_interest_rate,Time_in_years:").split()
+p=float(p)
+r=float(r)
+t=float(t)
+ci=(p*(1+r/100)**t)-p
+print("simple interest is:",ci)
